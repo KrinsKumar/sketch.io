0b9813460c6e90725afff46de5d11ac2270e8303
Krins <krinskumar@gmail.com>
2022-10-25 12:16:23 -0400

    added-makeWhite()

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ document.addEventListener('DOMContentLoaded', function() {
     // loading the DOM into the file
     let container = document.querySelector('.container');
     let slider = document.querySelector(`#sliderInput`);
+    let previousValue = slider.value
 
     // iniitial setup 
     document.querySelector(`.sliderValue`).innerHTML = slider.value;
-    makeGrid();
+    makeGrid(1);
     selectBlack();
+    selectReset()
 
     // if slider is changed
     slider.addEventListener('input', function() {    // when sliders value is changed
         document.querySelector(`.sliderValue`).innerHTML = slider.value;
         makeGrid();
         selection(document.querySelector(`#selected`));
+        previousValue = slider.value
     })
 
     // if options are clicked
@@ -39,16 +42,43 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // functions
 
-    function makeGrid(value = slider.value) {    // makes the grid for the selected range
-        // removes the previous grid
+    function makeGrid(flag = 0) {    // makes the grid for the selected range
+        value = slider.value
         let previousGrid = document.querySelector(`#grid-holder`);
-        if (previousGrid) {
-            if (previousGrid.className > value) { // adds in the grid
-                for (let i = int(previousGrid.className); i <= value; i++) {
-
+        if (flag === 0) {
+            if (parseInt(previousGrid.className) < value) { // adds in the grid
+                for (let i = 1; i <= parseInt(previousGrid.className); i++) {  // adds extra cells in existing rows
+                    let existingRow = document.querySelector(`.row-${i}`);
+                    for(let j = parseInt(previousGrid.className) + 1; j <= value; j++) {
+                        cell = document.createElement('div');
+                        cell.classList.add(`node-${i - 1}${j - 1}`);
+                        existingRow.appendChild(cell);    // appends the cells in the row
+                    }
                 }
-            } else if (previousGrid.className < value) {    // removes from the grid
+                for (let i = parseInt(previousGrid.className) + 1; i <= value; i++) {  // adds extra rows
+                    let row = document.createElement('div');
+                    row.classList.add(`row-${i}`);
+                    for(let j = 1; j <= value; j++) {
+                        cell = document.createElement('div');
+                        cell.classList.add(`node-${i - 1}${j - 1}`);
+                        row.appendChild(cell);    // appends the cells in the row
+                    }
+                    previousGrid.appendChild(row);    // appends the whole row in the div
+                }
+                previousGrid.classList.remove(previousValue);    // updates the class of the container
+                previousGrid.classList.add(value);
 
+            } else if (parseInt(previousGrid.className) > value) {    // removes from the grid
+                for (let i = 1; i < parseInt(previousGrid.className); i++) {  // removes extra cells in existing rows
+                    let existingRow = document.querySelector(`.row-${i}`);
+                    let lastCell = existingRow.querySelector(`.node-${i - 1}${parseInt(previousGrid.className) - 1}`);
+                    lastCell.remove();
+                }
+                for (let i = parseInt(previousGrid.className); i > value; i--) {  // adds extra rows
+                    previousGrid.querySelector(`.row-${i}`).remove();
+                }
+                previousGrid.classList.remove(previousValue);
+                previousGrid.classList.add(value);
             }
         } else {
             // makes and adds a brand new grid
@@ -59,15 +89,14 @@ document.addEventListener('DOMContentLoaded', function() {
                 row.classList.add(`row-${i}`);
                 for(let j = 1; j <= value; j++) {
                     cell = document.createElement('div');
-                    cell.classList.add(`draw-node`);
-                    cell.classList.add(`${i}${j}`);
+                    cell.classList.add(`node-${i - 1}${j - 1}`);
                     row.appendChild(cell);    // appends the cells in the row
                 }
                 grid.appendChild(row);    // appends the whole row in the div
             }
-            grid.classList.add(`${i}`);
+            grid.classList.add(value);
             container.appendChild(grid);    // appends the div in the DOM
-        }
+        } 
     }
 
     
@@ -86,7 +115,8 @@ document.addEventListener('DOMContentLoaded', function() {
             grid.querySelectorAll(`div`).forEach(function(row) {
                 row.querySelectorAll(`div`).forEach(function(cell) {
                     cell.onclick = function() {
-                        cell.onclick = cell.style.backgroundColor = `black`;
+                        if (cell.style.backgroundColor !== `white`) makeWhite(cell);
+                        else cell.style.backgroundColor = `black`;
                     }
                 })
             })
@@ -113,7 +143,8 @@ document.addEventListener('DOMContentLoaded', function() {
                         let red = Math.floor(Math.random() * 255);
                         let green = Math.floor(Math.random() * 255);
                         let blue = Math.floor(Math.random() * 255);
-                        cell.onclick = cell.style.backgroundColor = `rgb(${red}, ${blue}, ${green})`;
+                        if (cell.style.backgroundColor !== `white`) makeWhite(cell);
+                        else cell.style.backgroundColor = `rgb(${red}, ${blue}, ${green})`;
                     }
                 })
             })
@@ -132,8 +163,11 @@ document.addEventListener('DOMContentLoaded', function() {
         selection(document.querySelector(`#selected`));
     }
 
+    function makeWhite(cell) {
+        cell.style.backgroundColor = `white`;
+    }
+
 })
 
-
-// stopped at line 48 was about to add the logic for change in the size of the grid
+// there is a glitch in the slider make grid when numbers are skipped in the slider
 // needed - invert, blur, erase, removeBorders 
