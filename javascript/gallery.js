e274fb7a47454064558f87b1d7f2f8b06177f1e4
Krins <krinskumar@gmail.com>
2022-10-27 20:43:02 -0400

    optimized-js

---
+++ javascript/gallery.js
@@ -0,0 +1,22 @@
+/*
+    Structure => gallery = array of objects(pictures)
+    pictures have :- length = int
+                     array of cells called rows
+                     array of rows called grid
+
+                     cells is an object that containes rgb values of the cells 
+                     
+
+    gallery returns one object from the array
+*/
+
+
+let gallery = []
+function returngallery() {
+    
+
+
+
+}
+
+export { gallery } 
---
+++ javascript/index.js
@@ -0,0 +1,174 @@
+//import { gallery } from `gallery.js`;
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    // loading the DOM into the file
+    let container = document.querySelector('.container');
+    let slider = document.querySelector(`#sliderInput`);
+    let previousValue = parseInt(slider.value);
+    let selected = 'black';
+
+
+    // iniitial setup 
+    document.querySelector(`.sliderValue`).innerHTML = slider.value;
+    makeGrid(1);    // dynamically makes the grid
+    let grid = document.querySelector(`#grid-holder`);
+    addDrawEvent()
+    resetTheCells();    // makes sure that every cell is white
+
+
+    // if slider is changed
+    slider.addEventListener('input', function() {    // when sliders value is changed
+        document.querySelector(`.sliderValue`).innerHTML = slider.value;
+        makeGrid();
+        previousValue = slider.value    // updates the previous grid
+        previousValue = parseInt(previousValue);
+        addDrawEvent();
+    })
+
+
+    // if a cell is clicked
+    let mouseDown = false
+    document.body.onmousedown = () => (mouseDown = true)
+    document.body.onmouseup = () => (mouseDown = false)
+
+
+    // if a button to change the option is clicked
+    document.querySelectorAll('.button-color').forEach( function(but) {
+        but.onclick = function() {
+            let previousSelected = document.querySelector(`#selected`);
+            if (previousSelected) previousSelected.removeAttribute('id');
+            selected = but.classList[0];
+            document.querySelector(`.${selected}`).id = `selected`;
+        }
+    })
+    document.querySelector(`.reset`).onclick = resetTheCells;
+
+    
+    // if border button is pressed
+    document.querySelector(`#toggle-borders`).onclick = function() {
+        if (container.id === `not-border`) container.removeAttribute('id');
+        else container.id = `not-border`
+    }
+
+
+
+
+
+
+
+
+
+
+
+    // current functions
+
+    
+
+
+
+// needed - invert, blur, erase, removeBorders, lighter, darker
+
+
+
+// done functions
+
+    // makes the grid for the selected range
+    function makeGrid(flag = 0) {    
+        value = parseInt(slider.value);
+        let previousGrid = document.querySelector(`#grid-holder`);
+        if (flag === 0) {
+            if (previousValue < value) { // adds in the grid
+                for (let i = 1; i <= previousValue; i++) {  // adds extra cells in existing rows
+                    let existingRow = document.querySelector(`.row-${i}`);
+                    for(let j =  previousValue + 1; j <= value; j++) {
+                        cell = document.createElement('div');
+                        cell.classList.add(`node-${i - 1}${j - 1}`);
+                        cell.style.backgroundColor = `white`;
+                        existingRow.appendChild(cell);
+                    }
+                }
+                for (let i = previousValue + 1; i <= value; i++) {  // adds extra rows
+                    let row = document.createElement('div');
+                    row.classList.add(`row-${i}`);
+                    for(let j = 1; j <= value; j++) {
+                        cell = document.createElement('div');
+                        cell.classList.add(`node-${i - 1}${j - 1}`);
+                        cell.style.backgroundColor = `white`;
+                        row.appendChild(cell);    // appends the cells in the row
+                    }
+                    previousGrid.appendChild(row);    // appends the whole row in the div
+                }
+                previousGrid.classList.remove(previousValue);    // updates the class of the container
+                previousGrid.classList.add(value);
+            } else if (previousValue > value) {    // removes from the grid
+                for (let i = previousValue; i > value; i--) {  // adds extra rows
+                    previousGrid.querySelector(`.row-${i}`).remove();
+                }
+                for (let i = 1; i <= value; i++) {  // removes extra cells in existing rows
+                    let existingRow = document.querySelector(`.row-${i}`);
+                    for(let j = value + 1; j <= previousValue; j++) { 
+                        let lastCell = existingRow.querySelector(`.node-${i - 1}${j - 1}`);
+                        lastCell.remove();
+                    }
+                }
+                previousGrid.classList.remove(previousValue);
+                previousGrid.classList.add(value);
+            }
+        } else {
+            // makes and adds a brand new grid
+            let grid = document.createElement('div');
+            grid.id = `grid-holder`;
+            for(let i = 1; i <= value; i++) {    // makes the cells
+                let row = document.createElement('div');
+                row.classList.add(`row-${i}`);
+                for(let j = 1; j <= value; j++) {
+                    cell = document.createElement('div');
+                    cell.classList.add(`node-${i - 1}${j - 1}`);
+                    row.appendChild(cell);    // appends the cells in the row
+                }
+                grid.appendChild(row);    // appends the whole row in the div
+            }
+            grid.classList.add(value);
+            container.appendChild(grid);    // appends the div in the DOM
+        } 
+    }
+
+    // resets the grids
+    function resetTheCells() {    
+        let grid = document.querySelector(`#grid-holder`);
+        if (grid) {
+            grid.querySelectorAll(`div`).forEach(function(row) {
+                row.querySelectorAll(`div`).forEach(function(cell) {
+                    cell.onclick = cell.style.backgroundColor = `white`;
+                })
+            })
+        }
+    }
+
+    // changes the color of the clicked cell
+    function changeColor(e) {
+        if (e.type === 'mouseover' && !mouseDown) return
+        if (selected === `black`) e.target.style.backgroundColor = `black`;
+        if (selected === `random`) {
+            let red = Math.floor(Math.random() * 255);
+            let green = Math.floor(Math.random() * 255);
+            let blue = Math.floor(Math.random() * 255);
+            e.target.style.backgroundColor = `rgb(${red}, ${blue}, ${green})`;
+        }
+        if (selected === `eraser`) e.target.style.backgroundColor = `white`
+    }
+
+    function addDrawEvent() {
+        grid.querySelectorAll(`div`).forEach(function(row) {
+            row.querySelectorAll(`div`).forEach(function(cell) {
+                cell.addEventListener('mouseover', changeColor);
+                cell.addEventListener('mousedown', changeColor);
+            })
+        })
+    }
+
+
+
+
+})
